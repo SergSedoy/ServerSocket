606771e5193eff2016ec19d2d5b21ed30d5b7fec
Serg Krivoshchekov <sergeysedoy2002@yandex.ru>
2021-03-30 23:36:12 +0300

    исправляем замечания 2: удаляем лишний try

--- src/main/java/serverSocket/Client.java
+++ src/main/java/serverSocket/Client.java
@@ -6,23 +6,20 @@ import java.net.Socket;
 public class Client {
 
     public static void main(String[] args) {
-        try {
-            try (Socket clientSocket = new Socket("localhost", Server.SERVER_PORT); // этой строкой мы запрашиваем у сервера доступ на соединение
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
-                System.out.println("Вы что-то хотели сказать? Введите это здесь:");
-                String name = reader.readLine(); // ждём пока клиент что-нибудь не напишет в консоль
-                out.write(name + "\n"); // отправляем сообщение на сервер
-                out.flush();
-                String serverWord = in.readLine(); // ждём, что скажет сервер
-                System.out.println(serverWord);
-            } finally {
-                System.out.println("Клиент был закрыт...");
-            }
+        try (Socket clientSocket = new Socket("localhost", Server.SERVER_PORT); // этой строкой мы запрашиваем у сервера доступ на соединение
+             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+            System.out.println("Вы что-то хотели сказать? Введите это здесь:");
+            String name = reader.readLine(); // ждём пока клиент что-нибудь не напишет в консоль
+            out.write(name + "\n"); // отправляем сообщение на сервер
+            out.flush();
+            String serverWord = in.readLine(); // ждём, что скажет сервер
+            System.out.println(serverWord);
         } catch (IOException e) {
             System.err.println(e);
+        } finally {
+            System.out.println("Клиент был закрыт...");
         }
-
     }
 }
--- src/main/java/serverSocket/Server.java
+++ src/main/java/serverSocket/Server.java
@@ -8,22 +8,20 @@ public class Server {
     public static final int SERVER_PORT = 7789;
 
     public static void main(String[] args) {
-        try {
-            try (ServerSocket server = new ServerSocket(SERVER_PORT);// серверсокет прослушивает порт 7789
-                 Socket clientSocket = server.accept()) {  // accept() будет ждать пока кто-нибудь не захочет подключиться
-                System.out.println("Сервер запущен!");
-                System.out.println("New connection accepted");
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
-                    final String name = in.readLine(); // ждём пока клиент что-нибудь нам напишет
-                    out.write(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-                    out.flush(); // выталкиваем все из буфера
-                }
-            } finally {
-                System.out.println("Сервер закрыт!");
+        try (ServerSocket server = new ServerSocket(SERVER_PORT);// серверсокет прослушивает порт 7789
+             Socket clientSocket = server.accept()) {  // accept() будет ждать пока кто-нибудь не захочет подключиться
+            System.out.println("Сервер запущен!");
+            System.out.println("New connection accepted");
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+                final String name = in.readLine(); // ждём пока клиент что-нибудь нам напишет
+                out.write(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                out.flush(); // выталкиваем все из буфера
             }
         } catch (IOException e) {
             System.err.println(e);
+        } finally {
+            System.out.println("Сервер закрыт!");
         }
     }
 }
